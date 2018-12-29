09ca3e3ec2d16f9ad652cca1d11edf5e00cfddca
AlexAldoshin <aaldoshin@gmail.com>
2018-12-30 00:37:54 +0300

    get data

--- WebIoT/Controllers/HomeController.cs
+++ WebIoT/Controllers/HomeController.cs
@@ -31,8 +31,9 @@ namespace WebIoT.Controllers
             return View();
         }
 
-        public EmptyResult RelaySet(int relay, byte set)
+        public JsonResult RelaySet(int relay, byte set)
         {
+            var ret = new Dictionary<string, byte>();
             using (var db = new Models.DBContext())
             {
                 var sel = db.NBIoTCommands.Where(p => (p.UserId == 9 && p.IdDev == 0)).FirstOrDefault();
@@ -42,9 +43,37 @@ namespace WebIoT.Controllers
                 sel.Data = CurCommand;
                 db.Entry(sel).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
+                ret.Add("Rel1", CurCommand[0]);
+                ret.Add("Rel2", CurCommand[1]);
             }
-            return null;
+            return Json(ret, JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult RelayGet()
+        {
+            var ret = new Dictionary<string, byte>();
+            using (var db = new Models.DBContext())
+            {
+                var sel = db.NBIoTCommands.Where(p => (p.UserId == 9 && p.IdDev == 0)).FirstOrDefault();
+                var CurCommand = sel.Data;                
+                ret.Add("Rel1", CurCommand[0]);
+                ret.Add("Rel2", CurCommand[1]);
+            }
+            return Json(ret, JsonRequestBehavior.AllowGet);
+        }        
+        public JsonResult LastDataGet()
+        {
+            var ret = new Dictionary<string, byte[]>();
+            using (var db = new Models.DBContext())
+            {
+                var sel = db.NBIoTDatas.Where(p => (p.UserId == 9 && p.IdDev == 0)).OrderByDescending(p=>p.Id).FirstOrDefault();
+                var CurCommand = sel.Data;
+
+                //ret.Add("BatLev", (float)(CurCommand[CurCommand.Length-3])/10);
+                ret.Add("LastData", CurCommand);
+
+            }
+            return Json(ret, JsonRequestBehavior.AllowGet);
+        }
     }
 }
--- WebIoT/Scripts/my/Start.js
+++ WebIoT/Scripts/my/Start.js
@@ -1,16 +1,49 @@
 ﻿
 $(function () {
     InitEvents();
+    //$('#R1ON').show(200);
+    //$('#R1OFF').show(300);
+    //$('#R2ON').show(400);
+    //$('#R2OFF').show(500);
+    
 });
 
-function InitEvents() {
+function InitEvents() {        
+    $.getJSON('/Home/RelayGet', RelaySetSuccess);
+    loadData();   
+    
     $('#R1ON').click({ relay: 1, set: 1 }, RelaySet); // на кнопку прикрутим обработчик
     $('#R1OFF').click({ relay: 1, set: 0 }, RelaySet); // на кнопку прикрутим обработчик
     $('#R2ON').click({ relay: 2, set: 1 }, RelaySet); // на кнопку прикрутим обработчик
     $('#R2OFF').click({ relay: 2, set: 0 }, RelaySet); // на кнопку прикрутим обработчик
-}
 
+    setInterval('loadData()', 5000);
+}
 function RelaySet(eventObj) {  
 
-    $.get('/Home/RelaySet', { relay: eventObj.data.relay, set: eventObj.data.set});
+    $.getJSON('/Home/RelaySet', { relay: eventObj.data.relay, set: eventObj.data.set }, RelaySetSuccess);
+}
+function RelaySetSuccess(data) {
+    if (data.Rel1 == 0) {
+        $('#R1ON').show(100);
+        $('#R1OFF').hide(100);
+    }
+    else {
+        $('#R1ON').hide(100);
+        $('#R1OFF').show(100);
+    }
+    if (data.Rel2 == 0) {
+        $('#R2ON').show(100);
+        $('#R2OFF').hide(100);
+    }
+    else {
+        $('#R2ON').hide(100);
+        $('#R2OFF').show(100);
+    }
+}
+function loadData() {
+    $.getJSON('/Home/LastDataGet', LastDataGetSuccess);
+}
+function LastDataGetSuccess(data) {
+    $('#BatLev').text("Напряжение батареи: " + data.LastData[45]/10 + " Вольт");
 }
