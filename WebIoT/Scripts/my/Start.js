0ae41330997f4345295d0092a7d498d3af81ef3c
AlexAldoshin <aaldoshin@gmail.com>
2018-12-30 21:53:16 +0300

    YaMap

--- WebIoT/Scripts/my/Start.js
+++ WebIoT/Scripts/my/Start.js
@@ -1,4 +1,28 @@
 ﻿
+
+var myMap;
+var SetCurrentPos=0;
+var DevLocation;
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init() {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [55.76, 37.64], // Москва
+        zoom: 10
+    }, {
+            searchControlProvider: 'yandex#search'
+        });
+    DevLocation = new ymaps.Placemark([0, 0]);
+    myMap.geoObjects.add(DevLocation);
+}
+
+
+
 $(function () {
     InitEvents();
     //$('#R1ON').show(200);
@@ -53,7 +77,16 @@ function LastDataGetSuccess(data) {
     text += "<p>Скорость:" + data.Speed_Over_Ground + "</p>";
     text += "<p>Курс:" + data.Course_Over_Ground + "</p>";
     text += "<p>Точность:" + ((data.HDOP + data.PDOP + data.VDOP) / 3).toFixed(1)  + "</p>";
-
     $('#BatLev').html(text);
 
+
+    if (SetCurrentPos == 0) {
+        myMap.setCenter([data.Latitude, data.Longitude]);
+        myMap.setZoom(18);
+        SetCurrentPos = data.Latitude + data.Longitude;
+    }    
+    DevLocation.geometry.setCoordinates([data.Latitude, data.Longitude]);
+
+
+    
 }
